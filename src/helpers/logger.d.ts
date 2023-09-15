1f705e04985bc09b6ea7af6c70e0aa11317d579d
Kostya Kostyushko <kostanos@gmail.com>
2023-09-15 11:40:30 +0200

    WVP-100 added loggerFactory

---
+++ src/helpers/logger.d.ts
@@ -0,0 +1,10 @@
+// yourLoggerFileName.d.ts
+
+import { Logger, SerializerFn } from 'pino';
+import { RequestHandler } from 'express';
+
+export function loggerFactory(fileUrl: string): Logger;
+
+export function expressPinoFactory(logger: Logger, ignorePaths?: string[]): RequestHandler;
+
+export const redaction: SerializerFn;
