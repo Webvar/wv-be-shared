f636f7ea394b75a59b52d07be045ce5ea5d07d42
Nazar Kuksov <nazar.kuksov@mindshine.com>
2024-08-23 05:24:07 +0300

    fix issue with rmq for reconnect

--- src/helpers/rmq/RabbitMQ.ts
+++ src/helpers/rmq/RabbitMQ.ts
@@ -142,6 +142,7 @@ export class RabbitMQ {
     if (!this.channels[entity.name]) {
       this.channels[entity.name] = new RabbitMQChannel(this.connection, {
         name: entity.name,
+        initiate: true,
       });
       lg.debug({ state: 'PUBLISH_CREATE_CHANNEL', entity: entity.name });
     }
@@ -169,6 +170,7 @@ export class RabbitMQ {
       this.channels[entity.name] = new RabbitMQChannel(this.connection, {
         name: entity.name,
         activityTimeout: 0,
+        initiate: true
       });
       lg.debug({ state: 'SUBSCRIBE_CREATE_CHANNEL', entity: entity.name });
     }
